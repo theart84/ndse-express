c6162ae7e695d59821cf4002fe17ea1ef3c46a4c
theart84 <artshadow74@gmail.com>
2021-02-14 13:56:26 +0500

    Task completed

--- controllers/BooksController.js
+++ controllers/BooksController.js
@@ -12,7 +12,7 @@ class BooksController {
 
   getBook(req, res) {
     const { id } = req.params;
-    const book = store.find((item) => item.id === id );
+    const book = store.find((item) => item.id === id);
     if (book) {
       res.status(200).json({
         success: true,
@@ -29,7 +29,6 @@ class BooksController {
   createBook(req, res) {
     const { title, description, authors, favorite, fileCover, fileName } = req.body;
     const book = new Book(title, description, authors, favorite, fileCover, fileName);
-    console.log(book);
     store.push(book);
     res.status(201).json({
       success: true,
@@ -38,11 +37,45 @@ class BooksController {
   }
 
   updateBook(req, res) {
-
+    const { title, description, authors, favorite, fileCover, fileName } = req.body;
+    const { id } = req.params;
+    const updateBookIndex = store.findIndex((item) => item.id === id);
+    if (updateBookIndex !== -1) {
+      store[updateBookIndex] = {
+        ...store[updateBookIndex],
+        title,
+        description,
+        authors,
+        favorite,
+        fileCover,
+        fileName
+      }
+      res.status(200).json({
+        success: true,
+        data: store[updateBookIndex]
+      })
+    } else {
+      res.status(404).json({
+        success: false,
+        message: 'Book not found'
+      })
+    }
   }
 
   deleteBook(req, res) {
-
+    const { id } = req.params;
+    const deleteBookIndex = store.findIndex((item) => item.id === id);
+    if (deleteBookIndex !== -1) {
+      store.splice(deleteBookIndex, 1);
+      res.status(200).json({
+        success: true
+      })
+    } else {
+      res.status(404).json({
+        success: false,
+        message: 'Book not found'
+      })
+    }
   }
 }
 
